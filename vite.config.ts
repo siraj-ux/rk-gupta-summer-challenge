cfcef7b89007973fca08c265656744a1b30c017a
Siraj <siraj@adx-clerator.com>
2026-03-28 23:43:59 +0530

    host in domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/rk-gupta-summer-challenge/",   // IMPORTANT for custom domain (webinar.coachrkgupta.com)
+  base: "/",   // IMPORTANT for custom domain (webinar.coachrkgupta.com)
 
   server: {
     host: "::",
